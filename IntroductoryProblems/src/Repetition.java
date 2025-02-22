3e3d83b5421c6df120b1efecb05cb20fb30aebe4
FOODOHOLIC <adarshkohli183@gmail.com>
2025-02-22 11:35:31 +0530

    new

--- IntroductoryProblems/src/Repetition.java
+++ IntroductoryProblems/src/Repetition.java
@@ -1,5 +1,24 @@
+import java.io.*;
+import java.util.*;
+
 public class Repetition {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        String S = br.readLine();
+
+        int maxLen = 1, currLen =1;
+        for (int i = 1; i < S.length(); i++) {
+            if(S.charAt(i)==S.charAt(i-1)){
+                currLen++;
+            }else{
+                maxLen = Math.max(maxLen, currLen);
+                currLen = 1;
+            }
+        }
+        maxLen = Math.max(maxLen,currLen);
+        pw.println(maxLen);
+        pw.flush();
     }
 }
