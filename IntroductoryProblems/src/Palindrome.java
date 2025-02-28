fe7e8afad43e2e839d810019fcdde257c3d12be9
FOODOHOLIC <adarshkohli183@gmail.com>
2025-02-28 21:25:01 +0530

    new

--- IntroductoryProblems/src/Palindrome.java
+++ IntroductoryProblems/src/Palindrome.java
@@ -6,57 +6,26 @@ public class Palindrome {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
 
-        String s = br.readLine();
-        int n = s.length();
+        int t = Integer.parseInt(br.readLine());
 
-        // Create frequency array
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'A']++;
-        }
+        while (t-- > 0) {
+            String line = br.readLine();
+            if (line == null) {
+                break; // Exit if there is no more input
+            }
+            StringTokenizer st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-        // Check if a palindrome is possible
-        int oddCount = 0;
-        char oddChar = 0;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] % 2 != 0) {
-                oddCount++;
-                oddChar = (char) (i + 'A');
+            // Check if the total coins are divisible by 3 and the maximum value is not greater than twice the minimum value
+            if ((a + b) % 3 == 0 && Math.max(a, b) <= 2 * Math.min(a, b)) {
+                pw.println("YES");
+            } else {
+                pw.println("NO"); // Use println instead of print
             }
         }
 
-        // If more than one character has an odd frequency, no palindrome is possible
-        if (oddCount > 1) {
-            pw.println("NO SOLUTION");
-            pw.flush();
-            return;
-        }
-
-        // Construct the left half of the palindrome
-        StringBuilder left = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            // Append the character if it hasn't been fully used yet
-            if (freq[c - 'A'] > 1) {
-                left.append(c);
-                freq[c - 'A'] -= 2;
-                // Reduce frequency by 2 (since we're using two characters)
-            }
-        }
-
-        // Construct the right half by reversing the left half
-        String right = left.reverse().toString();
-        left.reverse(); // Restore the original left half
-
-        // Append the middle character if there is one
-        String middle = (oddCount == 1) ? String.valueOf(oddChar) : "";
-
-        // Combine left, middle, and right to form the palindrome
-        String palindrome = left.toString() + middle + right;
-        pw.println(palindrome);
         pw.flush();
+        pw.close();
     }
 }
-
-
-
-
