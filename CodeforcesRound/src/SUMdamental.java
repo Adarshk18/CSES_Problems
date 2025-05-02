71237f83c684dee1fbfae4125a10fca2d937dad6
FOODOHOLIC <adarshkohli183@gmail.com>
2025-05-02 09:35:22 +0530

    new

--- CodeforcesRound/src/SUMdamental.java
+++ CodeforcesRound/src/SUMdamental.java
@@ -2,37 +2,38 @@ import java.util.Scanner;
 
 public class SUMdamental {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
         while (t-- > 0) {
-            int n = sc.nextInt();
-            int x = sc.nextInt();
-            if (n == 1) {
-                System.out.println(x != 0 ? x : -1);
-            } else if (n == 2) {
-                if (x == 0) {
-                    System.out.println(2);
-                } else if (x == 1) {
-                    System.out.println(5);
-                } else {
-                    System.out.println(x + 3);
+            int n = in.nextInt();
+            int x = in.nextInt();
+
+            if (n==1){
+                if (x==0){
+                    System.out.println("-1");
+                }else{
+                    System.out.println(x);
                 }
-            } else {
-                if (x == 0) {
-                    if (n % 2 == 1) {
-                        System.out.println(n + 3);
-                    } else {
-                        System.out.println(n);
-                    }
-                } else {
-                    if (n % 2 == 1) {
-                        System.out.println(n - 1 + x);
-                    } else {
-                        System.out.println(n - 2 + x);
-                    }
+                continue;
+            }
+
+            int y = Integer.bitCount(x);
+            if (y>n){
+                System.out.println(x);
+                continue;
+            }
+
+            int ans = x + (n-y);
+            if ((n-y)%2!=0) {
+                if (x==0 || x==1){
+                    ans +=3;
+                }else{
+                    ans +=1;
                 }
             }
+            System.out.println(ans);
         }
-        sc.close();
+
     }
 }
