e2252b2c625e729d4fb69e4a06335b2e25fd518a
FOODOHOLIC <adarshkohli183@gmail.com>
2025-05-03 09:57:19 +0530

    new

--- CodeforcesRound/src/NeoEscape.java
+++ CodeforcesRound/src/NeoEscape.java
@@ -2,34 +2,53 @@ import java.util.*;
 
 public class NeoEscape {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt(); // Number of test cases
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
 
         while (t-- > 0) {
-            int n = sc.nextInt(); // Number of buttons
-            int[] a = new int[n];
+            int n = in.nextInt();
+            List<Integer> a = new ArrayList<>();
             for (int i = 0; i < n; i++) {
-                a[i] = sc.nextInt(); // Read button weights
+                a.add(in.nextInt());
             }
 
-            // Initialize the number of clones needed
-            int clones = 1;
-            int lastPressed = a[0]; // Start with the first button's weight
-
-            // Traverse through the button weights
-            for (int i = 1; i < n; i++) {
-                if (a[i] > lastPressed) {
-                    clones++; // New clone required if current button is greater
-                    lastPressed = a[i]; // Reset lastPressed to current weight
-                } else {
-                    lastPressed = a[i]; // Continue with the same clone
+            //Removing the consecutive duplicates
+            List<Integer> b = new ArrayList<>();
+            int l = -1;
+            for (int i = 0; i < n; i++) {
+                if (a.get(i)!=l){
+                    b.add(a.get(i));
                 }
+                l = a.get(i);
             }
 
-            // Output the minimum number of clones required
-            System.out.println(clones);
+            n = b.size();
+            List<int[]> vex = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                vex.add(new int[]{b.get(i),i});
+            }
+
+            //sort the array in decreasing order
+            vex.sort((x,y)-> Integer.compare(y[0],x[0]));
+
+            int[] chk = new int[n];
+            int ans =0;
+            for (int i = 0; i < n; i++) {
+                int j = vex.get(i)[1];
+                if (j>0 && chk[j-1]==1){
+                    chk[j] = 1;
+                    continue;
+                }
+                if (j<n-1 && chk[j+1]==1){
+                    chk[j]=1;
+                    continue;
+                }
+                chk[j] = 1;
+                ans++;
+            }
+            System.out.println(ans);
         }
 
-        sc.close(); // Close scanner
+
     }
 }
