c7c93c166e807c88e6f01211b3e6f02b1013f626
Adarshk18 <adarshkohli183@gmail.com>
2025-12-16 12:07:31 +0530

    new

--- DP/src/CountingTowers.java
+++ DP/src/CountingTowers.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class CountingTowers {
+    static final int MOD = 1_000_000_007;
+    static final int MAX = 1_000_000;
+
+    static long[] ans = new long[MAX + 1];
+
+
+    static void precompute() {
+        long full = 1, split = 1;
+        ans[1] = 2;
+
+        for (int i = 2; i <= MAX; i++) {
+            long newFull = (2 * full + split) % MOD;
+            long newSplit = (full + 4 * split) % MOD;
+
+            full = newFull;
+            split = newSplit;
+
+            ans[i] = (full + split) % MOD;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        precompute();
+
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            System.out.println(ans[n]);
+        }
+    }
 }
