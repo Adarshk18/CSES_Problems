32dc982acaa31ad47f420ccd90049887b5e9594b
Adarshk18 <adarshkohli183@gmail.com>
2025-12-22 09:13:20 +0530

    new

--- DP/src/LIS.java
+++ DP/src/LIS.java
@@ -3,23 +3,35 @@ import java.util.*;
 
 public class LIS {
 
-    public static int lis(int[] arr){
+    public static int lis(int[] arr) {
+
+        //remember due to contraints we cannot solve this using classic dp : TC ~ O(n2) which is not possible for 2x10^5
+        //so will solve using greedy + binary search in O(nlogn)
+
+
         int n = arr.length;
+        int[] tails = new int[n];
+        int size = 0;
 
-        int[][] dp = new int[n+1][n+1];
+        for (int x : arr) {
+            int left = 0, right = size;
 
-        for (int ind = n-1; ind >= 0; ind--) {
-            for (int prevInd = ind-1; prevInd >= -1; prevInd--) {
-                int len = dp[ind+1][prevInd+1];
-                if (prevInd==-1 || arr[ind] > arr[prevInd]){
-                    len = Math.max(len, 1+dp[ind+1][ind+1]);
-                }
-                dp[ind][prevInd+1] = len;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (tails[mid] < x)
+                    left = mid + 1;
+                else
+                    right = mid;
             }
 
+            tails[left] = x;
+            if (left == size) size++;
         }
-        return dp[0][0];
+
+        return size;
+
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
