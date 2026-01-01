678367feb5082f2d0eae5b915806d498efe36fa7
Adarshk18 <adarshkohli183@gmail.com>
2026-01-01 13:02:12 +0530

    fix cloudinary peer deps for render

--- Tree/src/TreeDiameter.java
+++ Tree/src/TreeDiameter.java
@@ -4,36 +4,73 @@ import java.util.*;
 
 public class TreeDiameter {
     static List<Integer>[] tree;
-    static boolean[] visited;
-    static int farthestNode = 0;
-    static int maxD = 0;
 
-    static void dfs(int node, int distance){
-        visited[node] = true;
 
-        if (distance > maxD){
-            maxD = distance;
-            farthestNode = node;
-        }
+    static int bfs(int start, int n) {
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] dist = new int[n + 1];
 
-        for(int neighbour: tree[node]){
-            if (!visited[neighbour]){
-                dfs(neighbour,distance+1);
+        q.add(start);
+        visited[start] = true;
+
+        int farthest = start;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            farthest = node;
+            for (int nei : tree[node]) {
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    dist[nei] = dist[node] + 1;
+                    q.add(nei);
+                }
             }
         }
+        return farthest;
+    }
+
+    static int bfsDist(int start, int n) {
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] dist = new int[n + 1];
+
+        q.add(start);
+        visited[start] = true;
+
+        int maxD = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int nei : tree[node]) {
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    dist[nei] = dist[node] + 1;
+                    maxD = Math.max(maxD, dist[nei]);
+                    q.add(nei);
+                }
+            }
+        }
+        return maxD;
     }
 
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n= Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        tree = new ArrayList[n+1];
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        tree = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             tree[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
@@ -41,16 +78,9 @@ public class TreeDiameter {
             tree[b].add(a);
         }
 
-        //first dfs
-        visited = new boolean[n+1];
-        maxD =0;
-        dfs(1,0);
+        int aa = bfs(1, n);
+        int da = bfsDist(aa, n);
 
-        //second dfs
-        visited = new boolean[n+1];
-        maxD = 0;
-        dfs(farthestNode,0);
-
-        System.out.println(maxD);
+        System.out.println(da);
     }
 }
