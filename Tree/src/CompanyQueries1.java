91e1cb5de1c1c293c06f81329f32e6e3f7f8dc3f
Adarshk18 <adarshkohli183@gmail.com>
2026-01-03 13:18:17 +0530

    new

--- Tree/src/CompanyQueries1.java
+++ Tree/src/CompanyQueries1.java
@@ -3,20 +3,20 @@ import java.util.*;
 
 public class CompanyQueries1 {
     static int n, q;
-    static long[][] up;
+    static int[][] up;
     static int LOG;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
+
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         q = Integer.parseInt(st.nextToken());
 
-        LOG = (int)Math.ceil(Math.log(n)/Math.log(2))+1;
+        LOG = (int)(Math.log(n)/Math.log(2)+1);
 
-        up = new long[n+1][LOG];
+        up = new int[n+1][LOG];
         up[1][0] = 0;
 
         st = new StringTokenizer(br.readLine());
@@ -27,26 +27,25 @@ public class CompanyQueries1 {
         for (int j = 1; j < LOG; j++) {
             for (int i = 1; i <= n; i++) {
                 if (up[i][j-1]!=0){
-                    up[i][j] = up[(int) up[i][j-1]][j-1];
+                    up[i][j] = up[up[i][j-1]][j-1];
                 }
             }
         }
 
-        for (int i = 0; i < q; i++) {
+        StringBuilder sb = new StringBuilder();
+        while (q-- > 0) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            pw.println(kthAncestor(x,y));
+            int k = Integer.parseInt(st.nextToken());
+            sb.append(kthAncestor(x,k)).append("\n");
         }
-        pw.close();
-        br.close();
+        System.out.print(sb);
     }
 
     static int kthAncestor(int node, int k){
         for (int j = 0; j < LOG; j++) {
             if ((k & (1<<j))!=0){
-                node = (int) up[node][j];
+                node = up[node][j];
                 if (node==0) return -1;
             }
         }
